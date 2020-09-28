1984455f300f9064b3a9cfe254c020827aa2b6e4
Jéssica <jps936@hotmail.com>
2020-09-28 20:57:38 +0100

    Change protocol
    Change protocol to https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -16,7 +16,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
